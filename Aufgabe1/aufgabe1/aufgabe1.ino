1ba35dc5ff86a0ca1347035e56dbb24a01c47b8a
cyberkeiler <toby@terramultimedia.de>
2015-10-14 14:27:19 +0200

    Aufgabe 1

---
+++ Aufgabe1/aufgabe1/aufgabe1.ino
@@ -0,0 +1,22 @@
+/*
+ * Praktische Aufgabe 1:
+ * Auf dem Display soll ein Zählwerk sichbar werden, dass im Zahlenbereich 
+ * von -199 bis 999 arbeitet. Kommazahlen sind entsprechend zu integrieren. 
+ * Ob der Wechsel zwischen Auf- und Abzählen über einen Tastendruck 
+ * gesteuert wird oder eine (Arduino-)Zeitfunktion dafür benutzt wird, 
+ * bleibt Ihnen überlassen.
+ */
+
+ int cs = 33;
+ int sdc = 35;
+ int sdi = 34;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
