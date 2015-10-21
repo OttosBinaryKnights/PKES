a00221576afeba1bab412a6d0f2e8edc0ee213ab
cyberkeiler <toby@terramultimedia.de>
2015-10-22 01:22:08 +0200

    Kommentare und Formatierung angepasst

--- Aufgabe1/aufgabe1/aufgabe1.ino
+++ Aufgabe1/aufgabe1/aufgabe1.ino
@@ -16,30 +16,30 @@
  * Latch PC4
  */
 #define CLOCK_DELAY_MS 1
-int o = 0;
+int x = 0;
 
-void out(int x) {
+void out(int o) {
   //Startbit
   PORTC |= _BV(PORTC3);
   Clock();
-  o = (o +1) % 1000;
+
+  //Berechne die einzelnen Werte für Stellen 1-3
   int t = o;
   int d[3];
-  
   for (int i = 2; i >= 0; i--) {
     d[i] = t % 10;
-    t = t/10;
+    t = t / 10;
   }
 
   for (int i = 0; i <= 2; i++) {
+    //Gebe die Segmente der 3 Digits aus
     ShiftDigit( d[i] );
   }
-  
-  for (int i = 25; i < 35; i++) {
+
+  //Unbelegte Bits werden durchgeschaltet
+  for (int i = 25; i < 36; i++) {
     Clock();
   }
-  //36er Clock - Wird ausgegeben!
-  Clock();
 }
 
 //Erzeugt ein Clock-Cycle
@@ -50,22 +50,22 @@ void Clock(void) {
   PORTC &= ~_BV(PORTC2);
 }
 
-void ShiftDigit(int in){
-  
-   for (int i = 7; i >= 0; i--) {
+void ShiftDigit(int in) {
+  //Laufe durch die 8 Segmente des Digits und schalte entsprechend den Ausgang
+  for (int i = 7; i >= 0; i--) {
     byte dig = GetDigit(in);
-    if(((dig >> i)  & 0x01) != 1)
+    if (((dig >> i)  & 0x01) != 1)
       PORTC &= ~_BV(PORTC3);
     else
       PORTC |= _BV(PORTC3);
     Clock();
   }
-  
+
 }
 
 //Gibt die Segmentbelegung für jedes Digit zurück
-byte GetDigit(int in){
-  switch(in){
+byte GetDigit(int in) {
+  switch (in) {
     case 0: return 0b11111100;
     case 1: return 0b01100000;
     case 2: return 0b11011010;
@@ -88,10 +88,11 @@ int main (void)
   DDRC |= _BV(DDC4);  //LATCH
 
   while (1) {
-    for (int x = 9; x <= 24; x++) {
-      out(x);
-      _delay_ms(50);
-    }
+    out(x);
+    _delay_ms(50);
+    
+    // Erhöhe Testausgabe o um 1
+    x = (x + 1) % 1000;
   }
 }
 
