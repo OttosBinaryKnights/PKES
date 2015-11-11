bcc96979b7be6d321c3cce3c64e8732c72d1764a
cyberkeiler <toby@terramultimedia.de>
2015-11-11 14:51:10 +0100

    IMU Lib einfügen

---
+++ res/lib/imu/imu.cpp
@@ -0,0 +1,111 @@
+// MPU-9150 Accelerometer + Gyro + Compass + Temperature
+// -----------------------------
+//
+// By arduino.cc user "frtrobotik" (Tobias Hübner)
+//
+//
+// July 2013
+//      first version
+//
+// Open Source / Public Domain
+//
+// Using Arduino 1.0.1
+// It will not work with an older version,
+// since Wire.endTransmission() uses a parameter
+// to hold or release the I2C bus.
+//
+// Documentation:
+// - The InvenSense documents:
+//   - "MPU-9150 Product Specification Revision 4.0",
+//     PS-MPU-9150A.pdf
+//   - "MPU-9150 Register Map and Descriptions Revision 4.0",
+//     RM-MPU-9150A-00.pdf
+//   - "MPU-9150 9-Axis Evaluation Board User Guide"
+//     AN-MPU-9150EVB-00.pdf
+//
+// The accuracy is 16-bits.
+//
+// Some parts are copied by the MPU-6050 Playground page.
+// playground.arduino.cc/Main/MPU-6050
+// There are more Registervalues. Here are only the most
+// nessecary ones to get started with this sensor.
+#include <Arduino.h>
+#include <Wire.h>
+
+// Register names according to the datasheet.
+// According to the InvenSense document
+// "MPU-9150 Register Map and Descriptions Revision 4.0",
+#include "imu.h"
+
+//Variables where our values can be stored
+int cmps[3];
+int accl[3];
+int gyro[3];
+int temp;
+//http://pansenti.wordpress.com/2013/03/26/pansentis-invensense-mpu-9150-software-for-arduino-is-now-on-github/
+//Thank you to pansenti for setup code.
+//I will documented this one later.
+void MPU9150_setupCompass(){
+
+  MPU9150_writeSensor(0x0A, 0x00); //PowerDownMode
+  MPU9150_writeSensor(0x0A, 0x0F); //SelfTest
+  MPU9150_writeSensor(0x0A, 0x00); //PowerDownMode
+
+  MPU9150_writeSensor(0x24, 0x40); //Wait for Data at Slave0
+  MPU9150_writeSensor(0x25, 0x8C); //Set i2c address at slave0 at 0x0C
+  MPU9150_writeSensor(0x26, 0x02); //Set where reading at slave 0 starts
+  MPU9150_writeSensor(0x27, 0x88); //set offset at start reading and enable
+  MPU9150_writeSensor(0x28, 0x0C); //set i2c address at slv1 at 0x0C
+  MPU9150_writeSensor(0x29, 0x0A); //Set where reading at slave 1 starts
+  MPU9150_writeSensor(0x2A, 0x81); //Enable at set length to 1
+  MPU9150_writeSensor(0x64, 0x01); //overvride register
+  MPU9150_writeSensor(0x67, 0x03); //set delay rate
+  MPU9150_writeSensor(0x01, 0x80);
+
+  MPU9150_writeSensor(0x34, 0x04); //set i2c slv4 delay
+  MPU9150_writeSensor(0x64, 0x00); //override register
+  MPU9150_writeSensor(0x6A, 0x00); //clear usr setting
+  MPU9150_writeSensor(0x64, 0x01); //override register
+  MPU9150_writeSensor(0x6A, 0x20); //enable master i2c mode
+  MPU9150_writeSensor(0x34, 0x13); //disable slv4
+}
+
+////////////////////////////////////////////////////////////
+///////// I2C functions to get easier all values ///////////
+////////////////////////////////////////////////////////////
+
+int MPU9150_readSensor(int addrL, int addrH){
+  Wire.beginTransmission(MPU9150_I2C_ADDRESS);
+  Wire.write(addrL);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU9150_I2C_ADDRESS, 1, true);
+  byte L = Wire.read();
+
+  Wire.beginTransmission(MPU9150_I2C_ADDRESS);
+  Wire.write(addrH);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU9150_I2C_ADDRESS, 1, true);
+  byte H = Wire.read();
+
+  return (int16_t)((H<<8)+L);
+}
+
+int MPU9150_readSensor(int addr){
+  Wire.beginTransmission(MPU9150_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU9150_I2C_ADDRESS, 1, true);
+  return Wire.read();
+}
+
+int MPU9150_writeSensor(int addr,int data){
+  Wire.beginTransmission(MPU9150_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.write(data);
+  Wire.endTransmission(true);
+
+  return 1;
+}
