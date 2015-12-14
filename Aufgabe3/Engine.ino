ef087f432a8fca97571e39a8b540344d33618004
cyberkeiler <toby@terramultimedia.de>
2015-12-14 16:29:32 +0100

    New Functions

--- Aufgabe3/Engine.ino
+++ Aufgabe3/Engine.ino
@@ -19,21 +19,44 @@ void InitEngines()
     OCR4B  = 0; // Power RWD Right
 }
 
+void EngStopp(){
+  EngForward(0);
+}
+
 void EngForward(int speed){
   setEngine(0,speed);
   setEngine(1,speed);
 }
 
+void EngTurn(int dir,int speed){
+  setEngine(dir,speed);
+  setEngine(1-dir,speed*-1);
+}
+
 //if engineNum==0 -> right
 //   engineNum==1 -> left
 // speed -> value between 0 and 255 
+byte EngIn[4] = {0,0,0,0};
+
 void setEngine(boolean engineNum, int speed){
-  if(engineNum){
-    OCR1A  = speed; // Power FWD Left
-    OCR1B  = speed; // Power RWD Left
-  } else {
-    OCR4A  = speed; // Power FWD Right
-    OCR4B  = speed; // Power RWD Right
+  if (speed<0){
+    EngIn[0+engineNum*2] = speed;
+    EngIn[1+engineNum*2] = 0;
   }
+  else
+  {
+    EngIn[0+engineNum*2] = 0;
+    EngIn[1+engineNum*2] = -speed;
+  }
+  updateOCR();
 }
+
+
+void updateOCR(){
+    OCR1A  = EngIn[0];
+    OCR1B  = EngIn[1];
+    OCR4A  = EngIn[2];
+    OCR4B  = EngIn[3];
+}
+
 
