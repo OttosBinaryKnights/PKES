622854dbf9c77ba1c5a7aebf2581dcd160075680
SalomonSandrock <nike_1379@web.de>
2015-12-15 13:00:05 +0100

    setup

--- Aufgabe3/IMU.ino
+++ Aufgabe3/IMU.ino
@@ -18,6 +18,36 @@ void getIMUangle() {
   _delay_ms(100);
 }
 
+void IMU_setup(){
+
+    MPU9150_I2C_ADDRESS = 0x0C;      //change Address to Compass    
+
+    MPU9150_write(0x0A, 0x00); //PowerDownMode
+    MPU9150_write(0x0A, 0x0F); //SelfTest
+    MPU9150_write(0x0A, 0x00); //PowerDownMode
+
+    //MPU9150_I2C_ADDRESS = 0x68;      //change Address to MPU
+    MPU9150_I2C_ADDRESS = tempAddress; //new version to revert to original address
+
+    MPU9150_write(0x24, 0x40); //Wait for Data at Slave0
+    MPU9150_write(0x25, 0x8C); //Set i2c address at slave0 at 0x0C
+    MPU9150_write(0x26, 0x02); //Set where reading at slave 0 starts
+    MPU9150_write(0x27, 0x88); //set offset at start reading and enable
+    MPU9150_write(0x28, 0x0C); //set i2c address at slv1 at 0x0C
+    MPU9150_write(0x29, 0x0A); //Set where reading at slave 1 starts
+    MPU9150_write(0x2A, 0x81); //Enable at set length to 1
+    MPU9150_write(0x64, 0x01); //overvride register
+    MPU9150_write(0x67, 0x03); //set delay rate
+    MPU9150_write(0x01, 0x80);
+
+    MPU9150_write(0x34, 0x04); //set i2c slv4 delay
+    MPU9150_write(0x64, 0x00); //override register
+    MPU9150_write(0x6A, 0x00); //clear usr setting
+    MPU9150_write(0x64, 0x01); //override register
+    MPU9150_write(0x6A, 0x20); //enable master i2c mode
+    MPU9150_write(0x34, 0x13); //disable slv4
+}
+
 int getOffset(){
   int offset=0;
   for(int i=0;i<30;i++)
